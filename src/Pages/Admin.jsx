d0db58fe0ee7d1e3b91ab22f9fda9ae7b82a2f63
Ary0405 <aryansethia2003@gmail.com>
2023-05-12 11:54:14 +0530

    feat : more functioning to admin portal

--- src/Pages/Admin.jsx
+++ src/Pages/Admin.jsx
@@ -25,15 +25,22 @@ function Admin() {
       setUsers(usersTempArray);
     });
     const updateNav = () => {
-      if(localStorage.getItem('signin') === 'false' || localStorage.getItem('signin') === null) navigate('/')
-      else if (localStorage.getItem('signin') === 'true' && localStorage.getItem('admin') === 'true') {
-          document.getElementById('admin-link').classList.remove('disabled')
-          document.getElementById('admin-link').classList.add('enabled')
-          document.getElementById('jury-link').classList.remove('disabled')
-          document.getElementById('jury-link').classList.add('enabled')
-      }else  navigate('/jury')
-  };
-  updateNav();
+      if (
+        localStorage.getItem("signin") === "false" ||
+        localStorage.getItem("signin") === null
+      )
+        navigate("/");
+      else if (
+        localStorage.getItem("signin") === "true" &&
+        localStorage.getItem("admin") === "true"
+      ) {
+        document.getElementById("admin-link").classList.remove("disabled");
+        document.getElementById("admin-link").classList.add("enabled");
+        document.getElementById("jury-link").classList.remove("disabled");
+        document.getElementById("jury-link").classList.add("enabled");
+      } else navigate("/jury");
+    };
+    updateNav();
   }, []);
   const handleVerification = async (e, user) => {
     if (e.target.checked) {
@@ -46,39 +53,63 @@ function Admin() {
           if (doc.data()["registrationNumber"] === user["registrationNumber"]) {
             updateDoc(doc.ref, {
               verified: true,
+              eligible: true,
             });
           }
         });
       });
-    //   window.location.reload();
-    //   console.log(user);
+      //   window.location.reload();
+      //   console.log(user);
+    }
+  };
+  const handleUnVerification = async (e, user) => {
+    if (e.target.checked) {
+      alert("Are you sure you want to unverify this user?");
+
+      const userRef = collection(db, "users");
+      getDocs(userRef).then((snapshot) => {
+        snapshot.docs.forEach((doc) => {
+          if (doc.data()["registrationNumber"] === user["registrationNumber"]) {
+            updateDoc(doc.ref, {
+              verified: false,
+              eligible: null,
+            });
+          }
+        });
+      });
+    }
+  };
+  const rejectShortlist = async (e, user) => {
+    if (e.target.checked) {
+      alert("Are you sure you want to reject ");
+
+      const userRef = collection(db, "users");
+      getDocs(userRef).then((snapshot) => {
+        snapshot.docs.forEach((doc) => {
+          if (doc.data()["registrationNumber"] === user["registrationNumber"]) {
+            updateDoc(doc.ref, {
+              shortlist: false,
+              verified: false,
+              eligible: false,
+            });
+          }
+        });
+      });
     }
   };
 
-  const rejectShortlist = async (e, user) => {
-    if (e.target.checked) {
-        alert(
-            "Are you sure you want to reject "
-        );
-
-        const userRef = collection(db, "users");
-        getDocs(userRef).then((snapshot) => {
-            snapshot.docs.forEach((doc) => {
-                if (doc.data()["registrationNumber"] === user["registrationNumber"]) {
-                    updateDoc(doc.ref, {
-                        shortlist: false,
-                    });
-                }
-            });
-        });
-    }
-};
+  const handleClick = () => {
+    window.location.reload();
+  }
 
   return (
     <>
-      <Navbar/>
+      <Navbar />
       <div className="container">
-        <p className="h3 my-3">Admin Portal</p>
+        <div style={{"display" : "flex" , "justifyContent" : "space-between" , "alignItems" : "center"}}>
+          <p className="h3 my-3">Admin Portal</p>
+          <button onClick={handleClick} className="btn btn-primary">Update List</button>
+        </div>
         <p className="h5 my-4">Registraions Recieved : {users.length}</p>
         <p className="h5 my-4">Submissions - </p>
         <table class="table">
@@ -94,11 +125,12 @@ function Admin() {
               <th scope="col">Verification status</th>
               <th scope="col">Eligible</th>
               <th scope="col">Un Eligible</th>
+              <th scope="col">Marked as unverified</th>
             </tr>
           </thead>
           <tbody>
             {users.map((user) => {
-              if (user["submission"] === 1) {
+              if (user["submission"] === 1 && user["eligible"] !== false) {
                 return (
                   <tr>
                     <th scope="row">{user["registrationNumber"]}</th>
@@ -107,32 +139,124 @@ function Admin() {
                     <td>{user["dob"]}</td>
                     <td>{user["number"]}</td>
                     <td>
-                      <a href={user["aadhar_url"]} target="_blank" rel="noreferrer">View</a>
+                      <a
+                        href={user["aadhar_url"]}
+                        target="_blank"
+                        rel="noreferrer"
+                      >
+                        View
+                      </a>
                     </td>
                     <td>
-                      <a href={user["submission_url"]} target="_blank" rel="noreferrer">View</a>
+                      <a
+                        href={user["submission_url"]}
+                        target="_blank"
+                        rel="noreferrer"
+                      >
+                        View
+                      </a>
                     </td>
                     <td>
                       {user["verified"] === true ? (
                         <p>Verified</p>
-                        ) : ( 
-                          <p>Not Verified</p>
-                          )}
+                      ) : (
+                        <p>Not Verified</p>
+                      )}
                     </td>
                     <td>
                       <input
                         type="radio"
-                        name={'eligible'+user["registrationNumber"]}
+                        name={"eligible" + user["registrationNumber"]}
                         onChange={(e) => handleVerification(e, user)}
-                        />
+                      />
                     </td>
                     <td>
                       <input
                         type="radio"
-                        name={'eligible'+user["registrationNumber"]}
+                        name={"eligible" + user["registrationNumber"]}
                         onChange={(e) => rejectShortlist(e, user)}
-                        />
+                      />
                     </td>
+                    <td>
+                      <input
+                        type="radio"
+                        name={"eligible" + user["registrationNumber"]}
+                        onChange={(e) => handleUnVerification(e, user)}
+                      />
+                    </td>
+                  </tr>
+                );
+              }
+            })}
+          </tbody>
+        </table>
+        <br />
+        <hr />
+        <p className="h5 my-4">Marked Uneligible - </p>
+        <table class="table">
+          <thead>
+            <tr>
+              <th scope="col">Registration ID</th>
+              <th scope="col">Name</th>
+              <th scope="col">Email</th>
+              <th scope="col">DOB</th>
+              <th scope="col">Phone Number</th>
+              <th scope="col">Gov ID</th>
+              <th scope="col">Submission </th>
+              <th scope="col">Verification status</th>
+              <th scope="col">Marked as unverified</th>
+              {/* <th scope="col">Un Eligible</th> */}
+            </tr>
+          </thead>
+          <tbody>
+            {users.map((user) => {
+              if (user["submission"] === 1 && user["eligible"] === false) {
+                return (
+                  <tr>
+                    <th scope="row">{user["registrationNumber"]}</th>
+                    <td>{user["name"]}</td>
+                    <td>{user["email"]}</td>
+                    <td>{user["dob"]}</td>
+                    <td>{user["number"]}</td>
+                    <td>
+                      <a
+                        href={user["aadhar_url"]}
+                        target="_blank"
+                        rel="noreferrer"
+                      >
+                        View
+                      </a>
+                    </td>
+                    <td>
+                      <a
+                        href={user["submission_url"]}
+                        target="_blank"
+                        rel="noreferrer"
+                      >
+                        View
+                      </a>
+                    </td>
+                    <td>
+                      {user["eligible"] === false ? (
+                        <p>Marked Uneligible</p>
+                      ) : (
+                        <p>Not Verified</p>
+                      )}
+                    </td>
+                    <td>
+                      <input
+                        type="radio"
+                        name={"eligible" + user["registrationNumber"]}
+                        onChange={(e) => handleUnVerification(e, user)}
+                      />
+                    </td>
+                    {/* <td>
+                      <input
+                        type="radio"
+                        name={"eligible" + user["registrationNumber"]}
+                        onChange={(e) => rejectShortlist(e, user)}
+                      />
+                    </td> */}
                   </tr>
                 );
               }
--- src/Pages/Navbar.js
+++ src/Pages/Navbar.js
@@ -5,7 +5,7 @@ function Navbar() {
   return (
     <>
         <nav className="navbar navbar-expand-lg navbar-light bg-light">
-            <Link className="navbar-brand" to="/" id='home-link'>Home </Link>
+            <Link className="navbar-brand mx-3" to="/" id='home-link'>Home </Link>
             <Link className="navbar-brand disabled" to="/admin" id='admin-link' >Admin </Link>
             <Link className="navbar-brand disabled" to="/jury" id='jury-link'>Jury </Link>
         </nav>            
