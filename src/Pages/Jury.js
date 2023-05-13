549db91cd7109dedad9750b1bdeeb4f5f3d6f498
Ary0405 <aryansethia2003@gmail.com>
2023-05-13 12:07:47 +0530

    feat : jury marks

--- src/Pages/Jury.js
+++ src/Pages/Jury.js
@@ -12,6 +12,7 @@ const db = getFirestore(app);
 function Jury() {
     const usersRef = collection(db, "users");
     const [users, setUsers] = React.useState([]);
+    const [marks, setMarks] = React.useState('');
     const usersTempArray = [];
     const navigate = useNavigate();
 
@@ -73,17 +74,50 @@ function Jury() {
             });
         }
     };
+    const handleMarksSubmit = (user) => {
+        alert("Are you sure you want to submit the marks");
+        const userRef = collection(db, "users");
+        getDocs(userRef).then((snapshot) => {
+            snapshot.docs.forEach((doc) => {
+                if (doc.data()["registrationNumber"] === user["registrationNumber"]) {
+                    updateDoc(doc.ref, {
+                        marks: marks,
+                    });
+                }
+            });
+        });
+    }
+
+    const handleReEnterMarks = (user) => {
+        alert("Are you sure you want to re-enter the marks");
+        const userRef = collection(db, "users");
+        getDocs(userRef).then((snapshot) => {
+            snapshot.docs.forEach((doc) => {
+                if (doc.data()["registrationNumber"] === user["registrationNumber"]) {
+                    updateDoc(doc.ref, {
+                        marks: null,
+                    });
+                }
+            });
+        });
+    }
 
     return (
         <>
             <Navbar />
             <div className="container">
-                <p className="h3 my-3">Jury Page</p>
+                <div style={{ "display": "flex", "justifyContent": "space-between", "alignItems": "center" }}>
+                    <p className="h3 my-3">Jury Page</p>
+                    <button onClick={() => { window.location.reload() }} className='btn btn-primary'>Update List</button>
+                </div>
                 <p className="h5 my-4">
                     This is the Jury Page. Here you can view the submissions of the participants and shortlist them.<br /><br />
                     1. Click on the view button to view the submission of the participant.<br />
                     2. Click on the radio button to shortlist or reject the participant.<br />
-                    3. Once enough submissions, refresh the page to view the updated shortlisted status.
+                    3. You can also enter the marks of the participant.<br />
+                    4. Click on the re-enter marks button to re-enter the marks of the participant.<br />
+                    5. Click on the submit button to submit the marks of the participant.<br />
+                    6. Click on the update list button to update the recent changes made<br />
                 </p>
                 <table className='table'>
                     <thead>
@@ -93,6 +127,7 @@ function Jury() {
                             <th scope='col'>Shortlist Status</th>
                             <th scope='col'>Accept</th>
                             <th scope='col'>Reject</th>
+                            <th scope='col'>Marks</th>
                         </tr>
                     </thead>
                     <tbody>
@@ -121,6 +156,15 @@ function Jury() {
                                                 type='radio'
                                                 onChange={(e) => rejectShortlist(e, user)}
                                             />
+                                        </td>
+                                        <td>
+                                            {user["shortlist"] ?
+                                                user["marks"] ? <div style={{ "display": "flex", "alignItems": "center", "alignContent": "center" }}><p style={{ "marginRight": "30px" }}>{user["marks"]}</p> <button onClick={() => handleReEnterMarks(user)} className='btn btn-primary'>Re-enter Marks</button> </div> :
+                                                    <div className="d-flex">
+                                                        <input onChange={(e) => { setMarks(e.target.value) }} type="text" style={{ "marginRight": "30px" }} />
+                                                        <button onClick={() => handleMarksSubmit(user)} className="btn btn-primary">Submit</button>
+                                                    </div> : <p>Not Shortlisted</p>}
+
                                         </td>
                                     </tr>
                                 );
